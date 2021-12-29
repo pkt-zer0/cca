3a20fcad3b042cb2b7f4ca14bcd50f31938cf438
György Kovács <pktzerus@gmail.com>
2021-12-29 10:57:09 +0100

    Optional init/cleanup for anim updater

--- src/anim.ts
+++ src/anim.ts
@@ -1,5 +1,5 @@
-import { invalidate, render, scene, UICard } from './render';
-import { clamp } from 'lodash';
+import { invalidate, scene, UICard } from './render';
+import { clamp, noop } from 'lodash';
 
 export interface Animation {
     /** Called on each frame of an animation. Return true to indicate the animation has finished. */
@@ -40,13 +40,24 @@ export function scheduleAnimation(anim: Animation): void {
     currentAnimation = anim;
 }
 
+/** Allows defining an animation in terms of elapsed time. Optional callbacks for one-time init / cleanup logic. */
+interface AnimationCallbacks {
+    init?(): void;
+    update(elapsed: number): boolean;
+    cleanup?(): void;
+}
+
 /** Produces an animation callback from an updater function that works with elapsed time.
  *
  * The returned animation starts running when first invoked, and tracks elapsed time since then.
  */
-function animationUpdater(updater: any) {
+function animationUpdater(callbacks: AnimationCallbacks) {
     let prevTimestamp: number | undefined = undefined;
     let elapsed = 0;
+
+    const init = callbacks.init ?? noop;
+    const updater = callbacks.update;
+    const cleanup = callbacks.cleanup ?? noop;
 
     function update(timestamp: DOMHighResTimeStamp) {
         if (!prevTimestamp) {
@@ -55,7 +66,15 @@ function animationUpdater(updater: any) {
         const delta = timestamp - prevTimestamp;
         prevTimestamp = timestamp;
         elapsed += delta * animationSpeed;
-        return updater(elapsed);
+        if (elapsed === 0) {
+            init();
+            return false;
+        }
+        const done = updater(elapsed);
+        if (done) {
+            cleanup();
+        }
+        return done;
     }
 
     return update;
@@ -74,17 +93,19 @@ export function animateParallel(anims: Animation[]): Animation {
 /** Tweens a card's highlight value from 0 to 1 over 500ms. */
 export function animateHighlight(card: UICard, reverse: boolean) {
     const duration = 500;
-    return animationUpdater((elapsed: DOMHighResTimeStamp) => {
-        // Animation over
-        if (elapsed >= duration) {
-            return true;
+    return animationUpdater({
+        update(elapsed: DOMHighResTimeStamp) {
+            // Animation over
+            if (elapsed >= duration) {
+                return true;
+            }
+
+            card.highlight = !reverse ?
+                clamp(elapsed / duration, 0, 1) :
+                clamp(1 - (elapsed / duration), 0, 1);
+
+            return false;
         }
-
-        card.highlight = !reverse ?
-            clamp(elapsed / duration, 0, 1) :
-            clamp(1 - (elapsed / duration), 0, 1);
-
-        return false;
     });
 }
 
@@ -94,30 +115,26 @@ export function animateEnergyChange(amount: number) {
     const duration = 1600;
     const fadeout = duration - stable;
 
-    return animationUpdater((elapsed: DOMHighResTimeStamp) => {
-        // Skipped in turbo mode
-        if (turboMode) {
-            scene.energyChangeOpacity = 0;
-            return true;
-        }
+    return animationUpdater({
+        init() { scene.energyChange = amount; },
+        cleanup() { scene.energyChangeOpacity = 0; },
+        update(elapsed: DOMHighResTimeStamp) {
+            // Skipped in turbo mode, otherwise ends after duration has passed
+            if (turboMode || elapsed >= duration) {
+                return true;
+            }
 
-        scene.energyChange = amount; // TODO: Only set this once
-        // Animation over
-        if (elapsed >= duration) {
-            scene.energyChangeOpacity = 0;
-            return true;
-        }
+            // Three phases for the animation: fade in, stay, fade out
+            if (elapsed < rampUp) {
+                scene.energyChangeOpacity = clamp(elapsed / rampUp, 0, 1);
+            } else if (elapsed < stable) {
+                scene.energyChangeOpacity = 1;
+            } else {
+                const fadeoutElapsed = elapsed - stable;
+                scene.energyChangeOpacity = clamp(1 - fadeoutElapsed / fadeout, 0, 1);
+            }
 
-        // Three phases for the animation: fade in, stay, fade out
-        if (elapsed < rampUp) {
-            scene.energyChangeOpacity = clamp(elapsed / rampUp, 0, 1);
-        } else if (elapsed < stable) {
-            scene.energyChangeOpacity = 1;
-        } else {
-            const fadeoutElapsed = elapsed - stable;
-            scene.energyChangeOpacity = clamp(1 - fadeoutElapsed / fadeout, 0, 1);
+            return false;
         }
-
-        return false;
     });
 }
